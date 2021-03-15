b1c0bd301d3dd66705631dc54dbbc46775c35cca
Sahil Rajput <sahilrajput03@gmail.com>
2021-03-15 15:35:31 +0530

    add feat9 ~ sahil

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 let log = console.log;
 
 app.get("/", (req, res) => {
-  res.send("My blog app, new feat added, 66!");
+  res.send("My blog app, new feat added, 66 + 99!");
   // res.status(201).send('If you want to send custom status code.');
 });
 
